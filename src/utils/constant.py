1e4d970a1a4bd514c646b45514a9343a161a0dfe
Gulilil <juan.csantoso@gmail.com>
2024-06-12 23:50:08 +0700

    minor fix

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -3,7 +3,7 @@
 from type.point import Point
 import math
 
-UPPER_THRESHOLD = 10000
-LOWER_THRESHOLD = 5000
+UPPER_THRESHOLD = 100000
+LOWER_THRESHOLD = 50000
 
 INFINITY_POINT = Point(math.inf, math.inf)
